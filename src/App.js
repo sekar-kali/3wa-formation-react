29f717ba6f95b580dfd169c5d01c838fa02b3e2c
MAMADOU SECK <mamadouseck@Mac-mini-de-MAMADOU.local>
2023-11-26 19:00:42 +0100

    change text character to english

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import React from 'react'
-
+import React from 'react';
+import Card from './components/Card';
+import data from './assets/data';
 export default function App() {
   return (
-    <div>App</div>
-  )
+    <main className="p-10">
+      <h1 className="text-2xl text-gray-100 text-center mb-10 uppercase font-mono">Characters ⚡️</h1>
+      <div className="grid md:grid-cols-3 gap-5 ">
+        {data.map((item) => (
+          <Card key={item.name} image={item.image} name={item.name} description={item.description} />
+        ))}
+      </div>
+    </main>
+  );
 }
---
+++ src/assets/data.js
@@ -0,0 +1,35 @@
+import gonImage from './images/gon-image.png';
+import killuaImage from './images/killua-image.png';
+import kurapikaImage from './images/kurapika-image.png';
+import leorioImage from './images/leorio-image.png';
+import hisokaImage from './images/hisoka-image.png';
+
+const hunterXHunterCharacters = [
+  {
+    name: 'Gon Freecss',
+    image: gonImage,
+    description: 'A young and talented hunter with a pure heart.',
+  },
+  {
+    name: 'Killua Zoldyck',
+    image: killuaImage,
+    description: "From an assassin's family, Gon's best friend.",
+  },
+  {
+    name: 'Kurapika',
+    image: kurapikaImage,
+    description: 'The last survivor of the Kurta clan, seeking to avenge his family.',
+  },
+  {
+    name: 'Leorio Paradinight',
+    image: leorioImage,
+    description: 'An aspiring doctor, he aims to help others.',
+  },
+  {
+    name: 'Hisoka Morow',
+    image: hisokaImage,
+    description: 'A mysterious and dangerous magician with his own agenda.',
+  },
+];
+
+export default hunterXHunterCharacters;
